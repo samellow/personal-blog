803401b6ed0c61fcf925189b47e23a5b716ce4c3
samellow <sammykaris48@gmail.com>
2024-05-12 13:09:06 +0300

    polished the ui

--- client/src/components/article/Article.jsx
+++ client/src/components/article/Article.jsx
@@ -6,7 +6,7 @@ import { MdOutlineEdit } from "react-icons/md"
 const Article = () => {
   return (
     <article className="article">
-      <div className="article-content">
+      <div className="article-description">
       <h2 className="title">Article Title</h2>
       <p className="excerpt">The articles excerpt ......</p>
       </div>
--- client/src/components/createArticle/CreateArticle.jsx
+++ client/src/components/createArticle/CreateArticle.jsx
@@ -9,7 +9,7 @@ const CreateArticle = () => {
             <input type="text" name="title" id="title" className='article-title' placeholder='Title' />
             <textarea name="excerpt" id="excerpt" cols="150" rows="10"  placeholder='Enter short description' className='article-excerpt'></textarea>
             <textarea name="content" id="content" cols="150" rows="50" placeholder='Write blog' className='article-content'></textarea>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Publish" className='article-btn' />
         </form>
     </main>
   )
